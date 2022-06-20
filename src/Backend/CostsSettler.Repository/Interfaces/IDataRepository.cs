21e63fa82b232c19e4085410e17ed69609b9d914
Tomasz Terlecki <t.terlecki@wp.pl>
2022-06-20 14:43:50 +0200

    Data repository added

---
+++ src/Backend/CostsSettler.Repository/Interfaces/IDataRepository.cs
@@ -0,0 +1,29 @@
+﻿using CostsSettler.Model.Models;
+
+namespace CostsSettler.Repository.Interfaces
+{
+    public interface IDataRepository
+    {
+        Task<T> GetByIdAsync<T>(long id, IEnumerable<string> includes = null)
+            where T : BaseModel;
+
+        Task<IEnumerable<T>> GetManyByConditionAsync<T>(
+            Func<T, bool> condition = null,
+            IEnumerable<string> includes = null
+        ) where T : BaseModel;
+
+        Task<T> AddAsync<T>(T model) where T : BaseModel;
+
+        Task<IEnumerable<T>> AddRangeAsync<T>(IEnumerable<T> collection) where T : BaseModel;
+
+        Task<T> UpdateAsync<T>(T model) where T : BaseModel;
+
+        Task<T> RemoveAsync<T>(T model) where T : BaseModel;
+
+        Task<IEnumerable<T>> RemoveRangeAsync<T>(IEnumerable<T> collection) where T : BaseModel;
+
+        Task<bool> ContainsAsync<T>(T model) where T : BaseModel;
+
+        Task<int> CountAsync<T>(Func<T, bool> condition = null) where T : BaseModel;
+    }
+}
---
+++ src/Backend/CostsSettler.Repository/Repositories/DataRepository.cs
@@ -0,0 +1,104 @@
+﻿using CostsSettler.Model.Models;
+using CostsSettler.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CostsSettler.Repository.Repositories
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly CostsSettlerDbContext _dbContext;
+
+        public DataRepository(CostsSettlerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> GetByIdAsync<T>(
+            long id,
+            IEnumerable<string> includes = null
+        ) where T : BaseModel
+        {
+            var query = _dbContext.Set<T>().AsQueryable();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include).AsQueryable();
+
+            return await query.FirstOrDefaultAsync(model => model.Id == id);
+        }
+
+        public async Task<IEnumerable<T>> GetManyByConditionAsync<T>(
+            Func<T, bool> condition = null,
+            IEnumerable<string> includes = null
+        ) where T : BaseModel
+        {
+            var query = _dbContext.Set<T>().AsQueryable();
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query = query.Include(include).AsQueryable();
+
+            if (condition != null)
+                query = query.Where(condition).AsQueryable();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> AddAsync<T>(T model) where T : BaseModel
+        {
+            var result = _dbContext.Set<T>().Add(model).Entity;
+
+            await _dbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync<T>(IEnumerable<T> collection) where T : BaseModel
+        {
+            _dbContext.Set<T>().AddRange(collection);
+
+            await _dbContext.SaveChangesAsync();
+
+            return collection;
+        }
+
+        public async Task<T> UpdateAsync<T>(T model) where T : BaseModel
+        {
+            var result = _dbContext.Set<T>().Update(model).Entity;
+
+            await _dbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        public async Task<T> RemoveAsync<T>(T model) where T : BaseModel
+        {
+            var result = _dbContext.Set<T>().Remove(model).Entity;
+
+            await _dbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> RemoveRangeAsync<T>(IEnumerable<T> collection) where T : BaseModel
+        {
+            _dbContext.Set<T>().RemoveRange(collection);
+
+            await _dbContext.SaveChangesAsync();
+
+            return collection;
+        }
+
+        public async Task<bool> ContainsAsync<T>(T model) where T : BaseModel
+        {
+            return await _dbContext.Set<T>().ContainsAsync(model);
+        }
+
+        public async Task<int> CountAsync<T>(Func<T, bool> condition = null) where T : BaseModel
+        {
+            return condition == null
+                ? await _dbContext.Set<T>().CountAsync()
+                : _dbContext.Set<T>().Count(condition);
+        }
+    }
+}
